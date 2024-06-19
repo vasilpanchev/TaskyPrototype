8187b399de7a9c5a144db8c940fb051796427e70
Atanas Zlatanski <at.zlatanski@gmail.com>
2024-06-19 21:02:07 +0300

    TestingLayer ready

---
+++ TestingLayer/TestsUserCRUD.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer;
+using BusinessLayer;
+
+namespace TestingLayer
+{
+    public class TestsUserCRUD
+    {
+        UserContext userContext;
+
+        [Test]
+        public void TestCreateUser()
+        {
+            TaskyPrototypeContext taskyPrototypeContext = new TaskyPrototypeContext();
+            userContext = new UserContext(taskyPrototypeContext);
+            User testingUser = new User("Atanas", "examplePassword") { Username = "Atanas", Password = "examplePassword" };
+
+            userContext.Create(testingUser);
+
+            //Assert.AreEqual(userContext.Read(testingUser.Username), testingUser, "Create method isn't working");
+            Assert.That(testingUser, Is.EqualTo(userContext.Read(testingUser.Username)), "Create method for User isn't working");
+        }
+
+        public void TestReadUser()
+        {
+            TaskyPrototypeContext taskyPrototypeContext = new TaskyPrototypeContext();
+            userContext = new UserContext(taskyPrototypeContext);
+            User testingUser = new User("Atanas", "examplePassword") { Username = "Atanas", Password = "examplePassword" };
+
+            User userFromDB = userContext.Read(testingUser.Username);
+
+            //Assert.AreEqual(userFromDB, testingUser, "Read method isn't working");
+            Assert.That(testingUser, Is.EqualTo(userFromDB), "Read method for User isn't working");
+        }
+
+        public void TestUpdateUser()
+        {
+            TaskyPrototypeContext taskyPrototypeContext = new TaskyPrototypeContext();
+            userContext = new UserContext(taskyPrototypeContext);
+            User updatedUser = new User("Atanas", "updatedPasswordAtanas123") { Username = "Atanas", Password = "updatedPasswordAtanas123" };
+
+            userContext.Update(updatedUser);
+
+            //Assert.AreEqual(userContext.Read(updatedUser.Username, updatedUser, "Read method isn't working");
+            Assert.That(updatedUser, Is.EqualTo(userContext.Read(updatedUser.Username)), "Create method for User isn't working");
+        }
+        
+        [Test]
+        public void TestDeleteUser()
+        {
+            TaskyPrototypeContext taskyPrototypeContext = new TaskyPrototypeContext();
+            userContext = new UserContext(taskyPrototypeContext);
+            User testingUser = new User("Atanas", "updatedPasswordAtanas123") { Username = "Atanas", Password = "updatedPasswordAtanas123" };
+
+            userContext.Delete(testingUser.Username);
+
+            Assert.Throws<Exception>(() => userContext.Read(testingUser.Username));
+        }
+    }
+}
---
+++ TestingLayer/TestsUserTaskCRUD.cs
@@ -0,0 +1,64 @@
+﻿using BusinessLayer;
+using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingLayer
+{
+    internal class TestsUserTaskCRUD
+    {
+        UserTaskContext userTaskContext;
+
+        [Test]
+        public void TestCreateUser()
+        {
+            TaskyPrototypeContext taskyPrototypeContext = new TaskyPrototypeContext();
+            userTaskContext = new UserTaskContext(taskyPrototypeContext);
+            User testingUser = new User("Atanas", "examplePassword") { Username = "Atanas", Password = "examplePassword" };
+            UserTask testingTask = new UserTask("Testing Task", "", DateTime.Now, DateTime.Now, "Atanas") { Title = "Testing Task" };
+
+            userTaskContext.Create(testingUser, testingTask);
+
+            Assert.That(testingTask, Is.EqualTo(userTaskContext.Read(testingTask.TaskId)), "Create method for Task isn't working");
+        }
+
+        [Test]
+        public void TestReadUser()
+        {
+            TaskyPrototypeContext taskyPrototypeContext = new TaskyPrototypeContext();
+            userTaskContext = new UserTaskContext(taskyPrototypeContext);
+            UserTask testingTask = new UserTask("Testing Task", "", DateTime.Now, DateTime.Now, "Atanas") { Title = "Testing Task" };
+
+            UserTask taskFromDB = userTaskContext.Read(testingTask.TaskId);
+
+            Assert.That(testingTask, Is.EqualTo(taskFromDB), "Read method for Task isn't working");
+        }
+
+        [Test]
+        public void TestUpdateUser()
+        {
+            TaskyPrototypeContext taskyPrototypeContext = new TaskyPrototypeContext();
+            userTaskContext = new UserTaskContext(taskyPrototypeContext);
+            UserTask updatedTask = new UserTask("Testing Task", "", DateTime.Now, DateTime.Now, "Atanas") { Title = "Testing Task" };
+
+            userTaskContext.Update(updatedTask);
+
+            Assert.That(updatedTask, Is.EqualTo(userTaskContext.Read(updatedTask.TaskId)), "Create method for Task isn't working");
+        }
+
+        [Test]
+        public void TestDeleteUser()
+        {
+            TaskyPrototypeContext taskyPrototypeContext = new TaskyPrototypeContext();
+            userTaskContext = new UserTaskContext(taskyPrototypeContext);
+            UserTask testingTask = new UserTask("Testing Task", "", DateTime.Now, DateTime.Now, "Atanas") { Title = "Testing Task" };
+
+            userTaskContext.Delete(testingTask.TaskId);
+
+            Assert.Throws<Exception>(() => userTaskContext.Read(testingTask.TaskId));
+        }
+    }
+}
